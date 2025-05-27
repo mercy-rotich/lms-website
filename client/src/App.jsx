1752969b8cfc138043ec357080d7ed262365c214
mercy-rotich <cherotichm182@gmail.com>
2025-05-28 01:04:11 +0300

    created the student hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import AddCourse from './pages/educator/AddCourse'
 import MyCourses from './pages/educator/MyCourses'
 import StudentsEnrolled from './pages/educator/StudentsEnrolled'
 import Navbar from './components/student/Navbar'
+import Hero from './components/student/Hero'
 
 const App = () => {
 
